e30192c042fd75df2f339bd1c8793ff797f77a66
Rizky Nurachmad <rizky.nurachmad@detik.com>
2025-08-11 13:56:41 +0700

    add auto update db-ip

---
+++ update.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Set variables
+DB_FILE="/data/dbip/dbip-full.mmdb"                     # Path to your DB-IP .mmdb file
+PHP_PATH="/opt/php7/bin/php"                            # Path to PHP binary
+SCRIPT_PATH="/opt/new-ip/dbip-update.php"               # Path to your DB-IP update script
+REVIVE_PREFIX="revive"                                  # Prefix for revive servers
+REVIVE_COUNT=12                                         # Number of revive servers
+
+# Print current date and time
+echo "Date: $(date)"
+
+# Get old checksum
+old_checksum=$(md5sum "$DB_FILE" | awk '{ print $1 }')
+echo "Old checksum: $old_checksum"
+
+# Run the DB-IP update script
+echo "Updating DB-IP database..."
+$PHP_PATH $SCRIPT_PATH -d ip-to-location-isp -w -q
+
+# Get new checksum
+new_checksum=$(md5sum "$DB_FILE" | awk '{ print $1 }')
+echo "New checksum: $new_checksum"
+
+# Compare checksums
+if [[ "$old_checksum" != "$new_checksum" ]]; then
+    echo "Database has been updated."
+    echo ""
+    sleep 2s
+
+    echo "Copying to revive servers..."
+    for i in $(seq 1 $REVIVE_COUNT); do
+        server="${REVIVE_PREFIX}${i}"
+        echo "Syncing to $server..."
+        rsync -avhW --no-compress /data/project/dbip/{dbip-full.mmdb,*.json} "rsync://$server:12000/dbip/"
+    done
+
+    echo "All transfers complete."
+    echo ""
+    exit 0
+else
+    echo "Database is already up to date."
+    echo ""
+    exit 1
+fi
+
